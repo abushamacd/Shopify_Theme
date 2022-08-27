7609fea883afb8466f7edd66bd1e9049c6e93060
shama <assiddik001@gmail.com>
2022-08-27 11:34:45 +0600

    header done

--- ThmsPark_Fashion/assets/application.js
+++ ThmsPark_Fashion/assets/application.js
@@ -1,1 +1,12 @@
 // Put your application javascript here
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.padding = "15px 10px";
+        document.getElementById("logo").style.fontSize = "25px";
+    } else {
+        document.getElementById("navbar").style.padding = "30px 0px";
+        document.getElementById("logo").style.fontSize = "35px";
+    }
+}
